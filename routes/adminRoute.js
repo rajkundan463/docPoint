b1c70382a026b518ecb83cdf1c29a43900721fc6
Kundan <kraj37932@gmail.com>
2025-10-31 19:44:13 +0530

    Fix build path for Render deployment

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,5 +1,4 @@
-const express
- = require('express');
+const express= require('express');
 const router = express.Router();
 const User = require('../models/userModel');
 const Doctor = require('../models/doctorModel');
